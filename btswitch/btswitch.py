44fbec3ab126ca78a012e63ab2d025886d71cb03
Simon Krekels <simonkrekels@gmail.com>
2019-06-21 23:09:06 +0200

    show only relevant options

--- btswitch/btswitch.py
+++ btswitch/btswitch.py
@@ -3,13 +3,14 @@
 
 from albertv0 import *
 from shutil import which
+import subprocess as sp
 
 __iid__ = "PythonInterface/v0.1"
 __prettyname__ = "Bluetooth Manager"
 __version__ = "0.1"
 __trigger__ = "bt"
 __author__ = "Simon Krekels"
-__dependencies__ = ["rfkill","blueman-manager"]
+__dependencies__ = ["rfkill"]
 
 if which("rfkill") is None:
     raise Exception("'rfkill' not found in $PATH.")
@@ -24,34 +25,52 @@ def getIcon(arg):
     else:
         return iconLookup("blueman")
 
+def isActive():
+    var = sp.check_output(["rfkill", "list"]).split()[5].decode("utf-8")
+    if var == 'yes':
+        return False
+    elif var == 'no':
+        return True
+    else:
+        raise Exception("Unexpected output from 'rfkill list'.")
+
 def handleQuery(query):
 
     if query.isTriggered:
-        items = []
-        items.append(Item(
+        itemEnable = Item(
             id = __prettyname__,
             icon = getIcon("blue"),
             text = "Enable Bluetooth",
             subtext = "Enable Bluetooth",
             actions = [
                 ProcAction("Enable", ["rfkill", "unblock", "bluetooth"])
-                ]))
-        items.append(Item(
+                ])
+        itemDisable = Item(
             id = __prettyname__,
             icon = getIcon("grey"),
             text = "Disable Bluetooth",
             subtext = "Disable Bluetooth",
             actions = [
                 ProcAction("Disable", ["rfkill", "block", "bluetooth"])
-                ]))
-        items.append(Item(
+                ])
+        itemMenu = Item(
             id = __prettyname__,
             icon = getIcon("black"),
             text = "Manage Devices",
             subtext = "Open Device Manager",
             actions = [
                 ProcAction("Open", ["blueman-manager"])
-                ]))
+                ])
 
+        if isActive():
+            items = [
+                    itemDisable,
+                    itemMenu
+                    ]
+        else:
+            items = [
+                    itemEnable,
+                    itemMenu
+                    ]
 
         return items
